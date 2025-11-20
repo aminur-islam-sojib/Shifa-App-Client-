d6f61659f5f78e4e3cc2d942960317fd5461045b
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-11-21 00:51:48 +0600

    Add Shifa App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Shifa </h1>
+      <Button> Coming</Button>
+    </>
+  );
+}
+
+export default App;
